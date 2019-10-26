1d6ea2a6e21501ca9483ce9568891c7faaa749a0
Krzysztof <krzysztof.podmokly@gmail.com>
2019-10-26 21:14:57 +0200

    Dummy auth with Redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SAVE_COMMENT, FETCH_COMMENTS } from 'actions/types';
+import { SAVE_COMMENT, FETCH_COMMENTS, CHANGE_AUTH } from 'actions/types';
 
 export const saveComment = comment => {
   return {
@@ -14,5 +14,12 @@ export const fetchComments = () => {
   return {
     type: FETCH_COMMENTS,
     payload: response
+  };
+};
+
+export const changeAuth = isLoggedIn => {
+  return {
+    type: CHANGE_AUTH,
+    payload: isLoggedIn
   };
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
-const App = () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
+class App extends React.Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>Sign Out</button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>Sign In</button>
+      );
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/post'>Post a comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderHeader()}
+        <Route path='/post' component={CommentBox} />
+        <Route exact path='/' component={CommentList} />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { auth: state.auth };
 };
 
-export default App;
+export default connect(
+  mapStateToProps,
+  actions
+)(App);
